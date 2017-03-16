9a0ff0af4aa68568fbe1a63ed59829ecc9b929fd
AoEiuV020 <aoeiuv020@gmail.com>
2017-03-16 13:29:25 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+#重点都在/app/build.gradle里
+主要就是写了几个task,  
+首先要确保已经通过adb连接安卓，  
+
+##dx
+使用dx命令将所有jar打包成dex,  
+
+##push
+使用adb push将dex推到安卓里的/data/local/tmp目录下,  
+
+##dalvikvm
+使用adb shell调用安卓命令dalvikvm运行程序，  
---
+++ app/src/main/java/aoeiuv020/Main.java
@@ -0,0 +1,17 @@
+package aoeiuv020;
+import java.util.*;
+import java.io.*;
+/**
+ * @author AoEiuV020
+ * @version 1.0 , 2017/03/15
+ */
+public class Main{
+	public static void main(String[] args)throws Exception{
+		Main app=new Main();
+		app.testGuavaToString();
+	}
+	void testGuavaToString()throws Exception{
+		System.out.println("hello");
+	}
+}
+
